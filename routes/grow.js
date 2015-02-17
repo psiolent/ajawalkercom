5b41f6802264f99b3378dff3f0dd1e610405d038
Aja Walker <aja@ajawalker.com>
2015-02-16 21:26:32 -0600

    Not a fan of single quotes.

--- routes/grow.js
+++ routes/grow.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET the page */
-router.get('/', function(req, res, next) {
-    res.render('fullcanvas', { title: 'Grow', script: "grow" });
+router.get("/", function(req, res) {
+    res.render("fullcanvas", { title: "Grow", script: "grow" });
 });
 
 module.exports = router;
